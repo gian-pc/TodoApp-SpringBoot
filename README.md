73ac89a49c2c4d83a0110e76fd22e7f67770f72f
Gian Paucar Cortez <gpaucarcortez@gmail.com>
2024-04-23 23:25:44 -0500

    creamos otro endpoint - read

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ TodoApp SpringBoot
    - Se genera la clases Todo y TodoType dentro de la estructura domains
    - Además se agrega la anotación @Data de lombock
 3. controllers
-   - Creamos un primer endpoint en TodoTypeController
+   - Creamos un primer endpoint en TodoTypeController: http://localhost:8080/api/todoType/hello
+   - Creamos otro endpoint de lectura: http://localhost:8080/api/todoType/read
--- src/main/java/com/gianpc/restapis/controllers/TodoTypeController.java
+++ src/main/java/com/gianpc/restapis/controllers/TodoTypeController.java
@@ -1,5 +1,6 @@
 package com.gianpc.restapis.controllers;
 
+import com.gianpc.restapis.domains.TodoType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/todoType") // Mapea los web requests a /api/todoType
 public class TodoTypeController {
 
+
+
     @GetMapping("/hello") // Solo maneja peticiones GET
     public String hello() {
         return "Hello World from Spring Boot !";
+    }
+
+    /**
+     * expose GetMapping en el /read endpoint
+     * @return TodoType
+     */
+    @GetMapping("/read")
+    public TodoType readTodoType() {
+        TodoType todoType = new TodoType();
+        todoType.setCode("PERSONAL");
+        todoType.setDescription("Todo para personal stuff");
+        return todoType;
     }
 
 }
