8bd7f64dcc33ad8314af9774a636811a629f0324
Gian Paucar Cortez <gpaucarcortez@gmail.com>
2024-04-24 13:41:57 -0500

    definiendo los métodos en el controller

--- src/main/java/com/gianpc/restapis/controllers/TodoController.java
+++ src/main/java/com/gianpc/restapis/controllers/TodoController.java
@@ -4,6 +4,8 @@ import com.gianpc.restapis.domains.Todo;
 import com.gianpc.restapis.services.TodoService;
 import com.gianpc.restapis.services.TodoTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -28,27 +30,32 @@ public class TodoController {
     // No se ve la información que se envía a menos que se intercepte
     // Se recomienda usar HTTPS para que el response y el request esten cifrados
     @PostMapping
-    public Todo create(){
-        // ...
+    public Todo create(@RequestBody Todo todo){ // Recibe un objeto de tipo Todo y la anotación @RequestBody lo convierte a un objeto de tipo Todo
+        return todoService.create(todo);
     }
 
     // El @GetMapping si tiene un límite de consultas, por que depende de lo que mandes en el URL
     // GET solo puede mandar 2048 caracteres
     // Si se ve la información que se envía
-    @GetMapping
-    public Todo read(){
-
+    @GetMapping("/{id}")
+    public Todo read(@PathVariable("id") Long id){ // Lo que esta en el path eso que se llama id pásamelo a Long id
+        return todoService.findById(id);
     }
 
     // El PUT sirve para un update
     @PutMapping
-    public Todo update(){
-
+    public Todo update(@RequestBody Todo todo){
+        return todoService.update(todo);
     }
 
     // Para eliminar
-    @DeleteMapping
-    public void delete(){
-
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable("id") Long id){
+        try {
+            todoService.deleteById(id);
+            return new ResponseEntity(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
     }
 }
