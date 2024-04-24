f7962c7cf453fce02f798be3caba2f9892aff69e
Gian Paucar Cortez <gpaucarcortez@gmail.com>
2024-04-24 17:29:52 -0500

    Creando los métodos de los services

--- src/main/java/com/gianpc/restapis/services/TodoService.java
+++ src/main/java/com/gianpc/restapis/services/TodoService.java
@@ -3,12 +3,40 @@ package com.gianpc.restapis.services;
 import com.gianpc.restapis.domains.Todo;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service // proveernos una sola instancia SINGLETON de esta clase durante toda la ejecución de la aplicacion
 public class TodoService {
 
-    public List<Todo> findAll(){
-        return null;
+    private static Map<Long, Todo> todoCollection = new HashMap<>();
+    private static long idCount = 1L;
+
+
+    public Todo create(Todo todo) {
+        todo.setId(idCount);
+        todoCollection.put(idCount, todo);
+        idCount++;
+        return todo;
+    }
+
+    public Todo findById(Long id) {
+        return todoCollection.get(id);
+    }
+
+    public Todo update(Todo todo) {
+        todo.setLastUpdated(new Date());
+        if (todo.isDone()) {
+            todo.setDateDone(new Date());
+        }
+        todoCollection.put(todo.getId(), todo);
+        return todo;
+    }
+
+    public void deleteById(Long id) throws Exception{
+        if(todoCollection.remove(id) == null){
+            throw new Exception("No existe el " + id);
+        }
     }
 }
