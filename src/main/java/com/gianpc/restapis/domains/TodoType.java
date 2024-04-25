2a9ed9efe085fe418f0d4e0b7237e71db855cc91
Gian Paucar Cortez <gpaucarcortez@gmail.com>
2024-04-25 00:15:28 -0500

    Para no mostrar los null

--- src/main/java/com/gianpc/restapis/domains/TodoType.java
+++ src/main/java/com/gianpc/restapis/domains/TodoType.java
@@ -1,6 +1,8 @@
 package com.gianpc.restapis.domains;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import jakarta.xml.bind.annotation.XmlRootElement;
 import lombok.Data;
@@ -9,11 +11,14 @@ import java.util.Date;
 
 @Data
 @JsonPropertyOrder({"description", "code"}) // Para personalizar el orden de los campos en el JSON
+@JsonInclude(JsonInclude.Include.NON_NULL) // Para que no se muestren los campos nulos
 //@XmlRootElement // Para que pueda ser convertido a XML
 public class TodoType {
     private  String code;
     @JsonIgnore // Para que no se muestre en el JSON
     private String description;
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
     private Date dateCreated;
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
     private Date lastUpdated;
 }
