87440649dd3fd869bd7a1e203b29706eea17747f
Gian Paucar Cortez <gpaucarcortez@gmail.com>
2024-04-25 00:54:20 -0500

    Probando ciertas anotaciones

--- src/main/java/com/gianpc/restapis/domains/TodoType.java
+++ src/main/java/com/gianpc/restapis/domains/TodoType.java
@@ -1,21 +1,24 @@
 package com.gianpc.restapis.domains;
 
+
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import jakarta.xml.bind.annotation.XmlRootElement;
+
+import jakarta.xml.bind.annotation.*;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
-@JsonPropertyOrder({"description", "code"}) // Para personalizar el orden de los campos en el JSON
-@JsonInclude(JsonInclude.Include.NON_NULL) // Para que no se muestren los campos nulos
-//@XmlRootElement // Para que pueda ser convertido a XML
+@JsonPropertyOrder(alphabetic = true)
+//@JsonPropertyOrder({"description", "code"}) // Para personalizar el orden de los campos en el JSON
+//@JsonInclude(JsonInclude.Include.NON_NULL) // Para que no se muestren los campos nulos
+@XmlRootElement// Cambiamos el nombre del elemento raíz
+@XmlAccessorType(XmlAccessType.FIELD) // aplicar tecnica de serialización y deserilización
+@XmlType(propOrder = {"code", "dateCreated", "lastUpdated"}) // orden de los elementos en el XML
 public class TodoType {
     private  String code;
-    @JsonIgnore // Para que no se muestre en el JSON
+    @XmlTransient // Para que no se muestre en el XML
     private String description;
     @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
     private Date dateCreated;
