97b5fb190aceda203e8ed93c6b9015ff33597a0f
Gian Paucar Cortez <gpaucarcortez@gmail.com>
2024-04-25 00:01:52 -0500

    Muestra una salida ordenada alfabeticamente

--- src/main/java/com/gianpc/restapis/domains/TodoType.java
+++ src/main/java/com/gianpc/restapis/domains/TodoType.java
@@ -1,12 +1,14 @@
 package com.gianpc.restapis.domains;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import jakarta.xml.bind.annotation.XmlRootElement;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
-@XmlRootElement // Para que pueda ser convertido a XML
+@JsonPropertyOrder(alphabetic = true) // Para que se ordene alfabéticamente
+//@XmlRootElement // Para que pueda ser convertido a XML
 public class TodoType {
     private  String code;
     private String description;
