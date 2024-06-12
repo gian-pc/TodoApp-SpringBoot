095782b8e96d1610bde1c88eaf1e30b062187e0d
Gian Paucar Cortez <gpaucarcortez@gmail.com>
2024-06-11 23:22:25 -0500

    personalizar salida del json

--- src/main/java/com/gianpc/restapis/RestapisApplication.java
+++ src/main/java/com/gianpc/restapis/RestapisApplication.java
@@ -1,7 +1,12 @@
 package com.gianpc.restapis;
 
+import com.gianpc.restapis.domains.Todo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.RepresentationModelProcessor;
 
 // ComponentScan: controllers, services, repositories para agregar al classpath
 // EnableAutoConfiguration: Configura automaticamente el contexto de Spring Boot
@@ -15,4 +20,14 @@ public class RestapisApplication {
 		SpringApplication.run(RestapisApplication.class, args);
 	}
 
+	@Bean
+	public RepresentationModelProcessor<EntityModel<Todo>> todoProcessor(){
+		return new RepresentationModelProcessor<EntityModel<Todo>>(){
+			@Override
+			public EntityModel<Todo> process(EntityModel<Todo> model){
+				model.add(Link.of("http://localhost:8080/todoTypes", "types"));
+				return model;
+			}
+		};
+	}
 }
